bca6d690eaa54c7d74ce4dee4c50ae926d4af2b7
Sóstenes Apollo <ctr.Sostenes.DaSilva@prth.com>
2023-03-16 17:28:00 -0300

    server updates

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-// Remote server to receive in realtime messages from api and send to connected printers 
+// Remote server to receive in realtime messages from api and send to connected printers
 
-const activePrinters = new Map()
+const activePrintersBySocket = new Map()
+const activePrintersByCompanyBase = new Map()
+const activePrintersByCompany = new Map()
 
 // Realtime socket connection
 const server = require('http').createServer();
 const io = require('socket.io')(server);
-
 io.on('connection', async function(socket){
+    console.log(`Socket ${socket.id} connected`);
 
-  socket.on('message', async function(msg){
-    console.log(msg);
-  });
+    socket.emit('ask_for_register', {})
 
-  socket.on('disconnect', ()=>{ console.log('user disconnected');});
-})
+    socket.on('register_printer', data => { 
+      console.log(`Registering printer for ${socket.id} `, data);
+      activePrintersBySocket.set(socket.id, {...data, socket})
+      activePrintersByCompanyBase.set(data?.company_base_id, [...(activePrintersByCompanyBase.get(data?.company_base_id) || []), socket.id])
+      activePrintersByCompany.set(data?.company_id, [...(activePrintersByCompany.get(data?.company_id) || []), socket.id])
+    });
 
-server.listen(process.env.SOCKET_IO_PORT || 3006, ()=> {
-  console.log(`Waiting for connection socket io at ${process.env.SOCKET_IO_PORT || 3006}`)
+    socket.on('disconnect', (disconnected) => { 
+      console.log(`Socket ${socket.id} disconnected`);
+      const _socket = activePrintersBySocket.get(socket.id)
+      activePrintersBySocket.set(socket.id, null)
+      activePrintersByCompanyBase.set(_socket.company_base_id, [])
+      activePrintersByCompany.set(_socket.company_id, [])
+    });
+
+});
+
+server.listen(3001, ()=> {
+    console.log('Waiting for connection')
 });
 
 // Server
@@ -25,11 +39,30 @@ const fastify = require('fastify')({ logger: true })
 fastify.get('/', async (request, reply) => "up")
 
 fastify.get('/active', async (request, reply) => {
-  return Array.from(activePrinters)
+  return {
+    activePrintersBySocket: Array.from(activePrintersBySocket),
+    activePrintersByCompanyBase: Array.from(activePrintersByCompanyBase),
+    activePrintersByCompany: Array.from(activePrintersByCompany) 
+  }
 })
 
 fastify.post('/print', async (request, reply) => {
-  return request
+  request.log.info('some info')
+  const content = request.body.content
+  const company_base_id = request.body.company_base_id
+
+  const socketId = activePrintersByCompanyBase.get(company_base_id)
+  console.log('Socket', socketId);
+
+  if(!socketId) 
+    return {error: "Socket not found for company base "+company_base_id}
+
+  if(!activePrintersByCompanyBase.get(company_base_id)) 
+    return {error: "No socket connected."}
+  
+  io.to(socketId).emit('print', content)
+
+  return { result: "Printing content to company_base_id "+company_base_id+" socket "+activePrintersByCompanyBase.get(company_base_id) }
 })
 
 const start = async () => {
